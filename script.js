7cdbef2d94c967b194570c6b9e512254657110d2
Paulo <pshenrique2015@gmail.com>
2022-12-12 11:25:41 -0300

    correções de bugs.

--- script.js
+++ script.js
@@ -19,10 +19,17 @@ else
     
     for (let task of taskArray)
     {
-        inputTask.value = task;
-        createTask();
+        if (task != "")
+        {
+            inputTask.value = task;
+            createTask();
+            inputTask.value = "";
+            inputTask.focus();
+        }
     }
 }
+
+console.log(document.cookie);
 
 //fires button event when user press enter in input task field
 inputTask.addEventListener("keyup", event => {
@@ -35,161 +42,157 @@ inputTask.addEventListener("keyup", event => {
 
 //add new task
 btnAddTask.addEventListener("click", () => {
-    //if there is a duplicated task
-    if (tasks.includes(inputTask.value))
-    {
-        alert("It's not possible to insert a duplicated task!");
+    //trims user input.
+    inputTask.value = String(inputTask.value).trim();
 
-        inputTask.value = "";
-        inputTask.focus();
+    if (inputTask.value == "")
+    {
+        window.alert("It's not possible to insert an empty task!");
+    }
+    else if (tasks.split("@").some(taskText => taskText == inputTask.value)) //if there is a duplicated task
+    {
+        window.alert("It's not possible to insert a duplicated task!");
     }
     else
         createTask();
+
+    inputTask.value = "";
+    inputTask.focus();
 });
 
 function createTask()
 {
-    //trims user input.
-    inputTask.value = String(inputTask.value).trim();
-    
-    if (inputTask.value != "")
+    const newTask = document.createElement("li");
+
+    const taskSpan = document.createElement("span");
+    const h3Task = document.createElement("h3");
+    const btnRemoveTask = document.createElement("input");
+    const inputCheckSpan = document.createElement("input");
+
+    //if it is a new task
+    if (!tasks.split("@").some(taskText => taskText == inputTask.value))
     {
-        const newTask = document.createElement("li");
+        tasks = tasks != "" ? tasks + inputTask.value + "@" : inputTask.value + "@";
+        completedTasks = completedTasks != "" ? completedTasks + "false@" : "false@";
 
-        const taskSpan = document.createElement("span");
-        const h3Task = document.createElement("h3");
-        const btnRemoveTask = document.createElement("input");
-        const inputCheckSpan = document.createElement("input");
-
-        //if it is a new task
-        if (!tasks.includes(inputTask.value))
+        createCookie("tasks", tasks);
+        createCookie("completedTasks", completedTasks);
+    }
+    else
+    {
+        //sees if the task is marked as completed
+        let tasksArray = tasks.split("@");
+        let completedTasksArray = completedTasks.split("@");
+        
+        for (let i = 0; i < tasksArray.length; i++)
         {
-            tasks = tasks != "" ? tasks + inputTask.value + "@" : inputTask.value + "@";
-            completedTasks = completedTasks != "" ? completedTasks + "false@" : "false@";
-
-            createCookie("tasks", tasks);
-            createCookie("completedTasks", completedTasks);
-        }
-        else
-        {
-            //sees if the task is marked as completed
-            let tasksArray = tasks.split("@");
-            let completedTasksArray = completedTasks.split("@");
-            
-            for (let i = 0; i < tasksArray.length; i++)
+            if (tasksArray[i] == inputTask.value)
             {
-                if (tasksArray[i] == inputTask.value)
-                {
-                    inputCheckSpan.checked = completedTasksArray[i] == "true";
-                    break;
-                }
+                inputCheckSpan.checked = completedTasksArray[i] == "true";
+                break;
             }
         }
-        
-        inputCheckSpan.type = "checkbox";
+    }
+    
+    inputCheckSpan.type = "checkbox";
 
-        //changes task marker.
-        inputCheckSpan.addEventListener("change",  () => {
-            let tasksArray = tasks.split("@");
-            let completedTasksArray = completedTasks.split("@");
-          
+    //changes task marker.
+    inputCheckSpan.addEventListener("change",  () => {
+        let tasksArray = tasks.split("@");
+        let completedTasksArray = completedTasks.split("@");
+        
+        for (let i = 0; i < tasksArray.length; i++)
+        {
+            if (tasksArray[i] == inputCheckSpan.parentElement.parentElement.firstChild.nextSibling.textContent)
+            {
+                //changes in array
+                completedTasksArray[i] = completedTasksArray[i] == "true" ? "false" : "true";
+
+                //updates completed task
+                completedTasks = completedTasksArray.reduce((total, element) => {
+                    total += "@";
+
+                    return total + element;
+                });
+                break;
+            }
+        }
+
+        //updates cookies
+        createCookie("completedTasks", completedTasks);
+
+        markTask();
+    });
+
+    taskSpan.append(inputCheckSpan);
+    
+    h3Task.textContent = inputTask.value;
+
+    btnRemoveTask.type = "submit";
+    btnRemoveTask.value = "";
+
+    //removes a certain task from list.
+    btnRemoveTask.addEventListener("click", () => {
+        let tasksArray = tasks.split("@");
+        let completedTasksArray = completedTasks.split("@");
+        
+        //if the user wants to delete the only task remaning
+        if (completedTasksArray.length <= 2)
+            completedTasks = "";
+        else
+        {
             for (let i = 0; i < tasksArray.length; i++)
             {
-                if (tasksArray[i] == inputCheckSpan.parentElement.parentElement.firstChild.nextSibling.textContent)
+                if (tasksArray[i] == newTask.firstChild.nextSibling.textContent)
                 {
-                    //changes in array
-                    completedTasksArray[i] = completedTasksArray[i] == "true" ? "false" : "true";
+                    completedTasksArray[i] = "";
 
                     //updates completed task
+                    completedTasksArray = completedTasksArray.filter(taskCompletedText => taskCompletedText != "");
+                    
                     completedTasks = completedTasksArray.reduce((total, element) => {
                         total += "@";
 
                         return total + element;
                     });
+
+                    completedTasks += "@"
                     break;
                 }
             }
-
-            //updates cookies
-            createCookie("completedTasks", completedTasks);
-
-            markTask();
-        });
-
-        taskSpan.append(inputCheckSpan);
-        
-        h3Task.textContent = inputTask.value;
-
-        btnRemoveTask.type = "submit";
-        btnRemoveTask.value = "";
-
-        //removes a certain task from list.
-        btnRemoveTask.addEventListener("click", () => {
-            let tasksArray = tasks.split("@");
-            let completedTasksArray = completedTasks.split("@");
-            
-            //if the user wants to delete the only task reamaning
-            if (completedTasksArray.length == 2)
-                completedTasks = "";
-            else
-            {
-                for (let i = 0; i < tasksArray.length; i++)
-                {
-                    if (tasksArray[i] == newTask.firstChild.nextSibling.textContent)
-                    {
-                        completedTasksArray[i] = "";
-
-                        //updates completed task
-                        completedTasksArray = completedTasksArray.filter(taskCompletedText => taskCompletedText != "");
-
-                        completedTasks = completedTasksArray.reduce((total, element) => {
-                            total += "@";
-
-                            return total + element;
-                        });
-
-                        completedTasks += "@"
-                        break;
-                    }
-                }
-            }
-
-            createCookie("completedTasks", completedTasks);
-
-            //removes task from cookies
-            tasks = tasks.replace(newTask.firstChild.nextSibling.textContent + "@", "");
-            createCookie("tasks", tasks)
-            
-            //If the innertHtml is deleted, therefore all the content disappers.
-            newTask.innerHTML = "";
-            newTask.style.display = "none";
-        });
-
-        //appends elements of task
-        newTask.append(taskSpan);
-        newTask.append(h3Task);
-        newTask.append(btnRemoveTask);
-
-        markTask();
-
-        function markTask()
-        {
-            if (inputCheckSpan.checked)
-            {
-                h3Task.className = "finished-task";
-                ulTodo.append(newTask);
-            }
-            else
-            {
-                h3Task.className = "";
-                ulTodo.prepend(newTask);
-            }
         }
 
-        inputTask.value = "";
-        inputTask.focus();
+        createCookie("completedTasks", completedTasks);
+
+        //removes task from cookies
+        tasks = tasks.replace(newTask.firstChild.nextSibling.textContent + "@", "");
+        createCookie("tasks", tasks)
+        
+        //If the innertHtml is deleted, therefore all the content disappers.
+        newTask.innerHTML = "";
+        newTask.style.display = "none";
+    });
+
+    //appends elements of task
+    newTask.append(taskSpan);
+    newTask.append(h3Task);
+    newTask.append(btnRemoveTask);
+
+    markTask();
+
+    function markTask()
+    {
+        if (inputCheckSpan.checked)
+        {
+            h3Task.className = "finished-task";
+            ulTodo.append(newTask);
+        }
+        else
+        {
+            h3Task.className = "";
+            ulTodo.prepend(newTask);
+        }
     }
-    
 }
 
 //creates a cookie with no expire date
